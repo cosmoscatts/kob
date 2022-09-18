78e170ff04fef386d75c841e9bc0ee755f1f2e92
faitsse <2138889191@qq.com>
2022-09-19 00:37:03 +0800

    chore: init

---
+++ kob-backend/src/main/java/com/kob/backend/KobBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kob.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KobBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KobBackendApplication.class, args);
+    }
+
+}
