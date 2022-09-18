b93936fbf33f5469bdcabd237741a46610dfeae4
faitsse <2138889191@qq.com>
2022-09-19 00:37:03 +0800

    chore: init

---
+++ kob-backend/src/test/java/com/kob/backend/KobBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kob.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KobBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
