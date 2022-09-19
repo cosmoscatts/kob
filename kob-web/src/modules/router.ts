99088cb8266286cc68f913928e89d95073c050cf
faitsse <2138889191@qq.com>
2022-09-19 12:18:56 +0800

    chore: clean up

--- kob-web/src/modules/router.ts
+++
@@ -1,8 +0,0 @@
-import router, { createRouterGuard } from '~/router'
-import type { UserModule } from '~/types'
-
-export const install: UserModule = ({ use }) => {
-  createRouterGuard(router)
-  use(router)
-}
-
