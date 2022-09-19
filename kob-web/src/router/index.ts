a29d0d4cb653e0a84bf8d6a647c108d0e238f0a4
faitsse <2138889191@qq.com>
2022-09-19 13:21:12 +0800

    chore: update

--- kob-web/src/router/index.ts
+++ kob-web/src/router/index.ts
@@ -3,35 +3,35 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHashHistory('/'),
   routes: [
-    {
-      path: '/',
-      component: '',
-    },
-    {
-      path: '/login',
-      component: BLANK_LAYOUT,
-      children: [
-        {
-          path: '',
-          name: 'Login',
-          component: () => import('~/pages/login/index.vue'),
-          meta: {
-            requiresAuth: false,
-          },
-        },
-      ],
-    },
-    {
-      path: '/:pathMatch(.*)*',
-      component: BLANK_LAYOUT,
-      children: [
-        {
-          path: '',
-          name: 'NotFound',
-          component: () => import('~/pages/exception/not-found/index.vue'),
-        },
-      ],
-    },
+    // {
+    //   path: '/',
+    //   component: '',
+    // },
+    // {
+    //   path: '/login',
+    //   component: BLANK_LAYOUT,
+    //   children: [
+    //     {
+    //       path: '',
+    //       name: 'Login',
+    //       component: () => import('~/pages/login/index.vue'),
+    //       meta: {
+    //         requiresAuth: false,
+    //       },
+    //     },
+    //   ],
+    // },
+    // {
+    //   path: '/:pathMatch(.*)*',
+    //   component: BLANK_LAYOUT,
+    //   children: [
+    //     {
+    //       path: '',
+    //       name: 'NotFound',
+    //       component: () => import('~/pages/exception/not-found/index.vue'),
+    //     },
+    //   ],
+    // },
   ],
   scrollBehavior() {
     return { top: 0 }
--- kob-web/src/types/index.ts
+++
@@ -1,3 +0,0 @@
-import type { App } from 'vue'
-export type UserModule = (app: App) => void
-export type { App as AppContext }
