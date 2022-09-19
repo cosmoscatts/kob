f07dfed0aebe507b1d26dd4058bfeae325bc3b9d
faitsse <2138889191@qq.com>
2022-09-19 17:44:09 +0800

    chore: update

--- kob-web/src/config.ts
+++ kob-web/src/config.ts
@@ -1,23 +1,29 @@
 import type { Menu } from '~/types'
 
+/** 是否为 `debug` 模式 */
+export const debug = true
+
 /**
  * 项目基本信息
  */
 export const appMeta = {
-  appShortName: '',
-  appName: '',
-  description: '',
-  author: '',
-  github: '',
+  appShortName: 'KOB',
+  appName: 'King Of Bots',
+  description: '惊世骇俗的贪吃蛇双人对抗游戏',
+  author: 'duende',
+  github: 'https://github.com/dud9/kob',
 }
 
 /**
  * 项目基础布局
  */
 export const appLayout = {
-  navHeight: '',
-  contentPadding: '',
-  footHeight: '',
+  /** 导航栏高度 */
+  navHeight: '60px',
+  /** 内容区内边距 */
+  contentPadding: '20px',
+  /** 底部栏高度 */
+  footHeight: '50px',
 }
 
 /**
