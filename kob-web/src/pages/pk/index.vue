27143ae649b50ee80c7ec0a2dee74ce9d901bbfe
faitsse <2138889191@qq.com>
2022-09-20 10:21:44 +0800

    chore: update

--- kob-web/src/pages/pk/index.vue
+++ kob-web/src/pages/pk/index.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
 import GamePlayground from '~/components/GamePlayground.vue'
+import { appLayout } from '~/config'
+
+const { navHeight, footHeight, contentPadding } = appLayout
+
+const diffHeight = computed(() => {
+  return navHeight + footHeight + contentPadding * 2 + 1 + 1 + 3
+})
 </script>
 
 <template>
-  <div mt-1rem>
+  <div :style="{ minHeight: `calc(100vh - ${diffHeight}px)` }" flex-center>
     <GamePlayground />
   </div>
 </template>
