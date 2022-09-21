66810673cc2e08fa1c553585295fd51fd150d71c
faitsse <2138889191@qq.com>
2022-09-21 11:18:03 +0800

    chore: backend update

--- kob-backend/src/main/java/com/kob/backend/controller/user/UserController.java
+++ kob-backend/src/main/java/com/kob/backend/controller/user/UserController.java
@@ -1,6 +1,15 @@
 package com.kob.backend.controller.user;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/user")
-public class UserController {}
+import com.kob.backend.dataobject.UserDO;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/user")
+    public UserDO getUser() {
+        return null;
+    }
+}
