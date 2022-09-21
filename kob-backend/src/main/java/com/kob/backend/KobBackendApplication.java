768937ad26a552dca54f860ea2a7ac939e2d35e2
faitsse <2138889191@qq.com>
2022-09-21 14:10:55 +0800

    chore: backend update

--- kob-backend/src/main/java/com/kob/backend/KobBackendApplication.java
+++ kob-backend/src/main/java/com/kob/backend/KobBackendApplication.java
@@ -1,9 +1,11 @@
 package com.kob.backend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.kob")
+@SpringBootApplication
+@MapperScan("com.kob.backend.mapper")
 public class KobBackendApplication {
 
     public static void main(String[] args) {
---
+++ kob-backend/src/main/java/com/kob/backend/security/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.kob.backend.security;
+
+import java.util.Objects;
+
+import javax.annotation.Resource;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.kob.backend.dataobject.UserDO;
+import com.kob.backend.service.UserService;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Resource
+    private UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserDO user = userService.getOne(Wrappers.<UserDO>lambdaQuery().eq(UserDO::getUsername, username));
+        if (Objects.isNull(user))
+            throw new UsernameNotFoundException("用户不存在");
+
+        return new UserDetailsImpl(user);
+    }
+}
