feeecba3938c1cba731af23331b8ca438945e2c2
faitsse <2138889191@qq.com>
2022-09-22 09:42:53 +0800

    chore: backend update

--- kob-backend/src/main/java/com/kob/backend/biz/user/UserBizImpl.java
+++ kob-backend/src/main/java/com/kob/backend/biz/user/UserBizImpl.java
@@ -1,11 +1,34 @@
 package com.kob.backend.biz.user;
 
+import javax.annotation.Resource;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+
+import com.kob.backend.controller.user.vo.AccountReqVO;
+import com.kob.backend.controller.user.vo.AccountRespVO;
+import com.kob.backend.dataobject.UserDO;
+import com.kob.backend.security.UserDetailsImpl;
+import com.kob.backend.utils.JwtUtil;
 
 @Service
 public class UserBizImpl implements UserBiz {
+    @Resource
+    private AuthenticationManager authenticationManager;
+
     @Override
-    public void getToken() {
+    public AccountRespVO getToken(AccountReqVO accountReqVO) {
+        UsernamePasswordAuthenticationToken authenticationToken =
+            new UsernamePasswordAuthenticationToken(accountReqVO.getUsername(), accountReqVO.getPassword());
+        // 登录失败，会自动处理
+        Authentication authenticate = authenticationManager.authenticate(authenticationToken);
+        UserDetailsImpl loginUser = (UserDetailsImpl)authenticate.getPrincipal();
+        UserDO user = loginUser.getUser();
+        String jwt = JwtUtil.createJWT(user.getId().toString());
+
+        return new AccountRespVO().setToken(jwt);
 
     }
 }
