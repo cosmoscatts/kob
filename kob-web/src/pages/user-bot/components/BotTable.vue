7784a6c72fab493151fe9e9085037193e23d096a
faitsse <2138889191@qq.com>
2022-09-22 23:33:55 +0800

    chore: update

---
+++ kob-web/src/pages/user-bot/components/BotTable.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <n-card title="我的Bot" hoverable h-full>
+    <template #header-extra>
+      <n-button type="primary" text-color="white">
+        添加Bot
+      </n-button>
+    </template>
+    卡片内容
+    hhdhasd
+    <div>
+      dasdasd
+    </div>
+  </n-card>
+</template>
--- kob-web/src/pages/user-bot/index.vue
+++ kob-web/src/pages/user-bot/index.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts">
-
+import BotTable from './components/BotTable.vue'
+import BotPanel from './components/BotPanel.vue'
 </script>
 
 <template>
-  MyBot
+  <div
+    grid="~ cols-4" gap-x-5 lt-md="grid-rows-2 grid-cols-1"
+    w-70vw mx-a ha
+  >
+    <div col-span-1>
+      <BotPanel />
+    </div>
+    <div col-span-3 lt-md="col-span-1">
+      <BotTable />
+    </div>
+  </div>
 </template>
