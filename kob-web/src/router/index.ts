81b66d8eaf249945e6a04336dccccef33897a9de
faitsse <2138889191@qq.com>
2022-09-22 23:09:49 +0800

    chore: update

--- kob-web/src/router/index.ts
+++ kob-web/src/router/index.ts
@@ -35,6 +35,21 @@ const router = createRouter({
       ],
     },
     {
+      path: '/userBot',
+      component: BaseLayout,
+      children: [
+        {
+          path: '',
+          name: 'UserBot',
+          component: () => import('~/pages/user-bot/index.vue'),
+          meta: {
+            title: '我的Bot',
+            requiresAuth: true,
+          },
+        },
+      ],
+    },
+    {
       path: '/profile',
       component: BaseLayout,
       children: [
