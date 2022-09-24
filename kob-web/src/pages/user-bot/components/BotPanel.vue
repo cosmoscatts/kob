ed65b8b339cba6d7940f11a333d99790be69c40a
faitsse <2138889191@qq.com>
2022-09-24 12:07:01 +0800

    chore: update

--- kob-web/src/pages/user-bot/components/BotPanel.vue
+++ kob-web/src/pages/user-bot/components/BotPanel.vue
@@ -1,11 +1,27 @@
 <script setup lang="ts">
-
+const { user } = storeToRefs(useUserStore())
 </script>
 
 <template>
   <div ha>
-    <n-card title="我的Bot" hoverable>
-      卡片内容
+    <n-card hoverable>
+      <div flex="~ col" w-full flex-center>
+        <n-avatar
+          :style="{
+            width: '100%',
+            height: 'auto',
+            cursor: 'pointer',
+          }"
+          lt-md="max-w-200px"
+          size="large"
+          :src="user?.avatar"
+          @click="$router.push('/profile')"
+        />
+        <n-divider />
+        <div font-bold text-lg>
+          {{ user?.name || '-' }}
+        </div>
+      </div>
     </n-card>
   </div>
 </template>
