3af7e8cc6376b9d4a8267eba1e249b7e28d7f596
faitsse <2138889191@qq.com>
2022-10-10 23:20:19 +0800

    chore: update

--- backend-cloud/kob-backend/src/main/java/com/kob/backend/biz/pk/StartGameBizImpl.java
+++ backend-cloud/kob-backend/src/main/java/com/kob/backend/biz/pk/StartGameBizImpl.java
@@ -1,7 +1,10 @@
 package com.kob.backend.biz.pk;
 
+import org.springframework.stereotype.Service;
+
 import com.kob.backend.consumer.WebSocketServer;
 
+@Service
 public class StartGameBizImpl implements StartGameBiz {
     @Override
     public void startGame(Integer aId, Integer bId) {
