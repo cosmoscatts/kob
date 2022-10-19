9982b263916202d761a47d12ef5a8d251bec877c
duende <2138889191@qq.com>
2022-10-19 11:39:49 +0800

    chore: update

---
+++ kob-web/src/pages/profile/components/security/SecurityModal.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import SecurityPasswordForm from './SecurityPasswordForm.vue'
+
+const {
+  modalVisible = false,
+  formIndex = 0,
+  title = '安全设置',
+} = defineProps<{
+  modalVisible?: boolean
+  formIndex?: number
+  title?: string
+}>()
+
+const emit = defineEmits(['update:modalVisible'])
+
+const bodyStyle = {
+  width: '390px',
+}
+
+const securityActionCallback = () => {
+  emit('update:modalVisible', false)
+}
+
+provide('securityActionCallback', securityActionCallback)
+</script>
+
+<template>
+  <n-modal
+    :show="modalVisible"
+    size="huge"
+    preset="card"
+    :style="bodyStyle"
+    :bordered="true"
+    @close="$emit('update:modalVisible', false)"
+  >
+    <template #header>
+      <span text="1.5rem" font-bold>{{ title }}</span>
+    </template>
+    <SecurityPasswordForm v-if="formIndex === 0" />
+  </n-modal>
+</template>
