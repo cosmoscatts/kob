4ce4853a0684d5d97a8e27ea8c7cbc53b131aa2a
cosmoscatts <2138889191@qq.com>
2022-10-22 23:04:31 +0800

    chore: init acapp

---
+++ acapp/unocss.config.ts
@@ -0,0 +1,69 @@
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetUno,
+  presetWebFonts,
+  presetWind,
+} from 'unocss'
+
+export default defineConfig({
+  shortcuts: {
+    'hw-full': 'h-full w-full',
+    'hw-screen': 'h-screen w-screen',
+    'icon-btn': 'op30 bg-dark dark:bg-white hover:op100 hover:bg-primary_hover cursor-pointer',
+    'flex-center': 'flex justify-center items-center',
+    'flex-x-center': 'flex justify-center',
+    'flex-y-center': 'flex items-center',
+    'flex-col-center': 'flex-col flex-center',
+  },
+  presets: [
+    presetAttributify(),
+    presetUno(),
+    presetIcons({
+      scale: 1.2,
+    }),
+    presetWind(),
+    presetWebFonts({
+      fonts: {
+        sans: 'Inter',
+        mono: 'DM Mono',
+      },
+    }),
+  ],
+  theme: {
+    breakpoints: {
+      xs: '320px',
+      sm: '640px',
+      md: '1024px',
+      lg: '1280px',
+      xl: '1536px',
+      xxl: '1920px',
+    },
+    fontFamily: {
+      self: 'CMU Sans Serif, HKST',
+    },
+    colors: {
+      primary: 'var(--primary-color)',
+      primary_hover: 'var(--primary-color-hover)',
+      primary_pressed: 'var(--primary-color-pressed)',
+      primary_suppl: 'var(--primary-color-suppl)',
+      info: 'var(--info-color)',
+      info_hover: 'var(--info-color-hover)',
+      info_pressed: 'var(--info-color-pressed)',
+      info_suppl: 'var(--info-color-suppl)',
+      success: 'var(--success-color)',
+      success_hover: 'var(--success-color-hover)',
+      success_pressed: 'var(--success-color-pressed)',
+      success_suppl: 'var(--success-color-suppl)',
+      warning: 'var(--warning-color)',
+      warning_hover: 'var(--warning-color-hover)',
+      warning_pressed: 'var(--warning-color-pressed)',
+      warning_suppl: 'var(--warning-color-suppl)',
+      error: 'var(--error-color)',
+      error_hover: 'var(--error-color-hover)',
+      error_pressed: 'var(--error-color-pressed)',
+      error_suppl: 'var(--error-color-suppl)',
+    },
+  },
+})
---
+++ acapp/vite.config.ts
@@ -0,0 +1,51 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import Vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import DefineOptions from 'unplugin-vue-define-options/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import UnoCSS from 'unocss/vite'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '~/': `${resolve(__dirname, 'src')}/`,
+    },
+  },
+
+  plugins: [
+    UnoCSS(),
+
+    Vue({
+      reactivityTransform: true,
+    }),
+
+    DefineOptions(),
+
+    AutoImport({
+      imports: [
+        'vue',
+        'vue-router',
+        'vue/macros',
+        '@vueuse/head',
+        '@vueuse/core',
+        'pinia',
+      ],
+      dts: 'src/autoImports.d.ts',
+      dirs: [
+        'src/server/api',
+        'src/composables',
+        'src/store',
+      ],
+      vueTemplate: true,
+    }),
+
+    Components({
+      resolvers: [
+        NaiveUiResolver(),
+      ],
+      dts: 'src/components.d.ts',
+    }),
+  ],
+})
