420ebc75e7e535de39dcec88571bbcb7dde4c6b9
duende <2138889191@qq.com>
2022-11-08 19:33:39 +0800

    feat: logo

--- kob-web/src/layout/components/nav-items/NavLogo.vue
+++ kob-web/src/layout/components/nav-items/NavLogo.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import lottie from 'lottie-web'
 import { breakpointsTailwind } from '@vueuse/core'
 import { appMeta } from '~/config'
 
@@ -7,13 +8,24 @@ const { appName } = appMeta
 // 是否为移动端（包含 `PC` 端宽度过小的情况）
 const breakpoints = useBreakpoints(breakpointsTailwind)
 const hideTitle = breakpoints.between('sm', 'lg')
+
+onMounted(() => {
+  useTimeoutFn(() => {
+    lottie.loadAnimation({
+      container: document.querySelector('#lottie-logo')!,
+      path: 'https://assets10.lottiefiles.com/packages/lf20_ZsoSL7RsIe.json',
+      loop: true,
+      renderer: 'svg',
+    })
+  }, 50)
+})
 </script>
 
 <template>
   <div
     flex-center h-full cursor-pointer @click="$router.push('/')"
   >
-    <div i-ri-game-fill text="primary 24px" />
+    <div id="lottie-logo" h44px />
     <span v-if="!hideTitle" font="bold" pl-16px text-24px>
       {{ appName }}
     </span>
