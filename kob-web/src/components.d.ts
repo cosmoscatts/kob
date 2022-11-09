d4dc76e6341ce439d88e361600b15699780f44f5
duende <2138889191@qq.com>
2022-11-09 17:13:43 +0800

    chore: update

--- kob-web/src/components.d.ts
+++ kob-web/src/components.d.ts
@@ -18,6 +18,7 @@ declare module '@vue/runtime-core' {
     NCard: typeof import('naive-ui')['NCard']
     NConfigProvider: typeof import('naive-ui')['NConfigProvider']
     NDataTable: typeof import('naive-ui')['NDataTable']
+    NDatePicker: typeof import('naive-ui')['NDatePicker']
     NDivider: typeof import('naive-ui')['NDivider']
     NDrawer: typeof import('naive-ui')['NDrawer']
     NDropdown: typeof import('naive-ui')['NDropdown']
