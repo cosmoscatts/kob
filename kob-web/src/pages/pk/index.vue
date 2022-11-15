3c831e8a0bb43f2300b6047e280119d4c991cf8d
duende <2138889191@qq.com>
2022-11-15 20:21:45 +0800

    chore: update

--- kob-web/src/pages/pk/index.vue
+++ kob-web/src/pages/pk/index.vue
@@ -3,20 +3,31 @@ import { Machine, Match } from './components'
 import { appLayout } from '~/config'
 
 const { navHeight, footHeight, contentPadding } = appLayout
-const diffHeight = computed(() => navHeight + footHeight + contentPadding * 2 + 1 + 1 + 3)
+const diffHeight = computed(() => navHeight + footHeight + contentPadding * 2 + 1 + 1 + 6)
+
+const borderColor = computed(() => isDark.value ? 'rgba(255, 255, 255, 0.09)' : 'rgb(239, 239, 245)')
 
 let currentPageIndex = $ref(0)
-currentPageIndex = 1
+currentPageIndex = 0
 </script>
 
 <template>
   <div>
     <div
       v-if="currentPageIndex === 0"
-      flex="col center" border="1 solid []" w60vw
-      :style="{ minHeight: `calc(100vh - ${diffHeight}px)` }"
+      flex-y-center justify-between w60vw mxa rounded-10px
+      :style="{
+        minHeight: `calc(100vh - ${diffHeight}px)`,
+        border: `3px solid ${borderColor}`,
+        padding: '0 8vw',
+      }"
     >
-      1
+      <div w25vw h40vh text-5vw text-center lh-40vh border="1 red" cursor-pointer font-800 @click="currentPageIndex = 1">
+        <span text-7vw>匹</span>配
+      </div>
+      <div w25vw h40vh text-5vw text-center lh-40vh border="1 red" cursor-pointer font-800>
+        <span text-7vw>人</span>机
+      </div>
     </div>
     <Match v-if="currentPageIndex === 1" />
     <Machine v-if="currentPageIndex === 2" />
