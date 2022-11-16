69a416a55fd6523fa7dd4ebbed7366b17fafe441
duende <2138889191@qq.com>
2022-11-16 17:14:46 +0800

    chore: update

--- acapp/src/pages/pk/components/match/index.vue
+++ acapp/src/pages/pk/components/match/index.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import defaultAvatar from '~/assets/default-avatar.png'
-import ResultBoard from '~/components/ResultBoard.vue'
 
 const userStore = useUserStore()
 const { user } = storeToRefs(userStore)
