562174beebabdecd30794a7fce65199eda1a4d8d
cosmoscatts <2138889191@qq.com>
2022-12-24 08:48:57 +0800

    fix:【讨论区 emoji】

--- backend-cloud/kob-backend/src/main/java/com/kob/backend/biz/remark/RemarkBizImpl.java
+++ backend-cloud/kob-backend/src/main/java/com/kob/backend/biz/remark/RemarkBizImpl.java
@@ -1,17 +1,5 @@
 package com.kob.backend.biz.remark;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import javax.annotation.Resource;
-
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Service;
-
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
@@ -29,6 +17,17 @@ import com.kob.backend.security.UserDetailsImpl;
 import com.kob.backend.service.RemarkLikeService;
 import com.kob.backend.service.RemarkService;
 import com.kob.backend.service.UserService;
+import com.vdurmont.emoji.EmojiParser;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class RemarkBizImpl implements RemarkBiz {
@@ -42,11 +41,12 @@ public class RemarkBizImpl implements RemarkBiz {
     @Override
     public String addRemark(RemarkReqVO remarkReqVO) {
         UsernamePasswordAuthenticationToken authenticationToken =
-            (UsernamePasswordAuthenticationToken)SecurityContextHolder.getContext().getAuthentication();
-        UserDetailsImpl loginUser = (UserDetailsImpl)authenticationToken.getPrincipal();
+                (UsernamePasswordAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
+        UserDetailsImpl loginUser = (UserDetailsImpl) authenticationToken.getPrincipal();
         UserDO user = loginUser.getUser();
 
-        remarkReqVO.setUserId(user.getId()).setCreateTime(new Date());
+        remarkReqVO.setUserId(user.getId()).setCreateTime(new Date())
+                .setRemark(EmojiParser.parseToAliases(remarkReqVO.getRemark())); // 表情符号转字符
         remarkService.save(RemarkConverter.INSTANCE.vo2do(remarkReqVO));
         return null;
     }
@@ -61,11 +61,12 @@ public class RemarkBizImpl implements RemarkBiz {
             return PageMap.empty();
         }
 
-        List<RemarkRespVO> data = list.stream().map(RemarkConverter.INSTANCE::do2vo).collect(Collectors.toList());
-
+        List<RemarkRespVO> data = list.stream()
+                .map(item -> item.setRemark(EmojiParser.parseToUnicode(item.getRemark())))
+                .map(RemarkConverter.INSTANCE::do2vo).collect(Collectors.toList());
         List<Integer> remarkIdList = list.stream().map(RemarkDO::getId).collect(Collectors.toList());
         List<RemarkLikeDO> likeList =
-            remarkLikeService.list(Wrappers.<RemarkLikeDO>lambdaQuery().in(RemarkLikeDO::getRemarkId, remarkIdList));
+                remarkLikeService.list(Wrappers.<RemarkLikeDO>lambdaQuery().in(RemarkLikeDO::getRemarkId, remarkIdList));
         Map<Integer, Integer> likeMap = new HashMap<>();
         for (RemarkLikeDO likeDO : likeList) {
             Integer num = likeMap.getOrDefault(likeDO.getRemarkId(), 0) + 1;
@@ -74,14 +75,14 @@ public class RemarkBizImpl implements RemarkBiz {
 
         List<Integer> userIdList = list.stream().map(RemarkDO::getUserId).collect(Collectors.toList());
         List<RankRespVO> userList = userService.list(Wrappers.<UserDO>lambdaQuery().in(UserDO::getId, userIdList))
-            .stream().map(UserConverter.INSTANCE::do2RankVO).collect(Collectors.toList());
+                .stream().map(UserConverter.INSTANCE::do2RankVO).collect(Collectors.toList());
         Map<Integer, RankRespVO> userMap = new HashMap<>();
         for (RankRespVO rankRespVO : userList) {
             userMap.put(rankRespVO.getId(), rankRespVO);
         }
         for (RemarkRespVO respVO : data) {
             respVO.setLikes(likeMap.getOrDefault(respVO.getId(), 0))
-                .setUser(userMap.getOrDefault(respVO.getUserId(), null));
+                    .setUser(userMap.getOrDefault(respVO.getUserId(), null));
         }
 
         return PageMap.data(page.getTotal(), data);
@@ -90,13 +91,13 @@ public class RemarkBizImpl implements RemarkBiz {
     @Override
     public List<Integer> getCurrentUserLikes() {
         UsernamePasswordAuthenticationToken authenticationToken =
-            (UsernamePasswordAuthenticationToken)SecurityContextHolder.getContext().getAuthentication();
-        UserDetailsImpl loginUser = (UserDetailsImpl)authenticationToken.getPrincipal();
+                (UsernamePasswordAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
+        UserDetailsImpl loginUser = (UserDetailsImpl) authenticationToken.getPrincipal();
         UserDO user = loginUser.getUser();
 
         List<Integer> likes =
-            remarkLikeService.list(Wrappers.<RemarkLikeDO>lambdaQuery().eq(RemarkLikeDO::getLikeUserId, user.getId()))
-                .stream().map(RemarkLikeDO::getRemarkId).collect(Collectors.toList());
+                remarkLikeService.list(Wrappers.<RemarkLikeDO>lambdaQuery().eq(RemarkLikeDO::getLikeUserId, user.getId()))
+                        .stream().map(RemarkLikeDO::getRemarkId).collect(Collectors.toList());
         return likes;
     }
 
@@ -111,8 +112,8 @@ public class RemarkBizImpl implements RemarkBiz {
         }
 
         UsernamePasswordAuthenticationToken authenticationToken =
-            (UsernamePasswordAuthenticationToken)SecurityContextHolder.getContext().getAuthentication();
-        UserDetailsImpl loginUser = (UserDetailsImpl)authenticationToken.getPrincipal();
+                (UsernamePasswordAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
+        UserDetailsImpl loginUser = (UserDetailsImpl) authenticationToken.getPrincipal();
         UserDO user = loginUser.getUser();
 
         RemarkLikeDO like = new RemarkLikeDO().setRemarkId(remarkId).setLikeUserId(user.getId());
@@ -131,12 +132,12 @@ public class RemarkBizImpl implements RemarkBiz {
         }
 
         UsernamePasswordAuthenticationToken authenticationToken =
-            (UsernamePasswordAuthenticationToken)SecurityContextHolder.getContext().getAuthentication();
-        UserDetailsImpl loginUser = (UserDetailsImpl)authenticationToken.getPrincipal();
+                (UsernamePasswordAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
+        UserDetailsImpl loginUser = (UserDetailsImpl) authenticationToken.getPrincipal();
         UserDO user = loginUser.getUser();
 
         remarkLikeService.remove(Wrappers.<RemarkLikeDO>lambdaQuery().eq(RemarkLikeDO::getRemarkId, remarkId)
-            .eq(RemarkLikeDO::getLikeUserId, user.getId()));
+                .eq(RemarkLikeDO::getLikeUserId, user.getId()));
         return null;
     }
 }
