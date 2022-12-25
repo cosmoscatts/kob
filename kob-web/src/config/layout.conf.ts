5f08641cbe10226a74f15da01b5f55e62f957859
cosmoscatts <2138889191@qq.com>
2022-12-25 19:06:36 +0800

    chore: update

--- kob-web/src/config/layout.conf.ts
+++ kob-web/src/config/layout.conf.ts
@@ -1,9 +1,9 @@
 export const APP_LAYOUT_PARAMS = {
-  primaryColor: '#0d9488', // 主题色
-  navHeight: 60, // 导航栏高度
-  contentPadding: 30, // 内容区内边距
-  footHeight: 50, // 底部栏高度
-  backTopRight: 20, // backTop 距离页面右部的距离
-  backTopBottom: 200, // backTop 距离页面底部的距离
-  backTopvisibilityHeight: 250, // backTop 滚动时触发显示回到顶部的高度
+  primaryColor: '#0d9488',
+  navHeight: 60,
+  contentPadding: 30,
+  footHeight: 50,
+  backTopRight: 20,
+  backTopBottom: 200,
+  backTopvisibilityHeight: 250,
 }
--- kob-web/src/utils/color.ts
+++ kob-web/src/utils/color.ts
@@ -9,24 +9,12 @@ const ALPHA = 0.8
 export type RGBA = [number, number, number, number]
 export type RGB = [number, number, number]
 
-/**
- * 创建 Hover 颜色
- */
 export const createHoverColor = (color: string | RGB | RGBA) => composite(color, [255, 255, 255, 0.12])
 
-/**
- * 创建 Pressed 颜色
- */
 export const createPressedColor = (color: string | RGB | RGBA) => composite(color, [0, 0, 0, 0.15])
 
-/**
- * 给颜色加透明度
- */
 export const addColorAlpha = (color: string, alpha: number) => colord(color).alpha(alpha).toHex()
 
-/**
- * 生成主色调的其他状态颜色，包括 hover、pressed、suppl 等状态
- */
 export const createPrimaryColor = (primaryColor: string) => ({
   primaryColor,
   primaryColorHover: createHoverColor(primaryColor),
