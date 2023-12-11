d72739c1c9fbdd1c5838e61f712c5570427a8360
cosmoscatts <2138889191@qq.com>
2023-12-11 09:47:02 +0800

    chore: update

--- kob-web/src/layout/components/TheFoot.vue
+++ kob-web/src/layout/components/TheFoot.vue
@@ -3,7 +3,7 @@ import { APP_META } from '~/config'
 </script>
 
 <template>
-  <div flex-center font-bold text-14px op30>
-    {{ APP_META.copyRight }}
+  <div flex-center font-bold text-14px>
+    <span op30>{{ APP_META.copyRight }}</span><span mx-2 op30> · </span><a href="http://beian.miit.gov.cn/" op-50 target="_blank" text-primary hover="!op-100">苏ICP备2023051139号</a>
   </div>
 </template>
