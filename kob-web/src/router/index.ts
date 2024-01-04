2693b9e69103dff2742e5590abd3d06c6baaedfa
cosmoscatts <2138889191@qq.com>
2024-01-04 15:49:25 +0800

    chore: update

--- kob-web/src/router/index.ts
+++ kob-web/src/router/index.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import { createRouterGuard } from './guard'
 import routes from './format-routes'
 
 const router = createRouter({
-  history: createWebHashHistory('/'),
+  history: createWebHistory('/'),
   routes: [
     {
       path: '/',
