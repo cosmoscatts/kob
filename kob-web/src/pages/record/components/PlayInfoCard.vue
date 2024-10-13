9e25f02680c4c4e2cf88564c564fd9eac520d9b1
泠韵 <2138889191@qq.com>
2024-10-13 17:01:57 +0800

    Update

---
+++ kob-web/src/pages/record/components/PlayInfoCard.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import type { PlayerInfo } from '../columns';
+import defaultAvatar from '~/assets/default-avatar.png';
+
+defineProps<{
+  player: PlayerInfo
+  color: string
+  side: string
+}>();
+</script>
+
+<template>
+  <div flex="col center" gap-y-4>
+    <div :style="{ color }" text="18px" font-bold>
+      {{ side }}
+    </div>
+    <n-avatar
+      round
+      :size="60"
+      :src="player.avatar ?? defaultAvatar"
+    />
+    <n-ellipsis :style="{ maxWidth: '80px' }">
+      {{ player.name ?? '-' }}
+    </n-ellipsis>
+  </div>
+</template>
