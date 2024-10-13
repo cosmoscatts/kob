72bb50d23b4cdd6c3d7543514fbc945860374154
泠韵 <2138889191@qq.com>
2024-10-13 22:28:32 +0800

    pref

--- kob-web/src/pages/record/index.vue
+++ kob-web/src/pages/record/index.vue
@@ -4,29 +4,38 @@ import type { PageQuery } from '~/types';
 import RecordTable from './components/RecordTable.vue';
 import RecordVideo from './components/RecordVideo.vue';
 
+interface TabChangeParams {
+  pagination?: PageQuery
+  playerInfoList?: PlayerInfo[]
+  name?: string
+}
+
 const currentTab = ref(0);
-const name = ref<string | undefined>(''); // 搜索的玩家
-const pagination = ref<PageQuery>();
+const name = ref('');
+const pagination = ref<PageQuery>({});
 const playerInfoList = ref<PlayerInfo[]>([]);
 
 const route = useRoute();
+
 watch(() => route.path, () => {
   currentTab.value = 0;
-  name.value = undefined;
+  name.value = '';
   pagination.value = {};
   playerInfoList.value = [];
-});
+}, { immediate: true });
+
+const currentTabComponent = computed(() =>
+  currentTab.value === 0 ? RecordTable : RecordVideo,
+);
 
 function changeCurrentTab(
   tab: 0 | 1,
-  _pagination: PageQuery,
-  _playerInfoList: PlayerInfo[],
-  _name = undefined,
+  { pagination: _pagination, playerInfoList: _playerInfoList, name: _name }: TabChangeParams = {},
 ) {
-  if (tab === 1) { // 保存表格的分页数据
-    name.value = _name;
-    pagination.value = _pagination;
-    playerInfoList.value = _playerInfoList;
+  if (tab === 1) {
+    name.value = _name ?? '';
+    pagination.value = _pagination ?? {};
+    playerInfoList.value = _playerInfoList ?? [];
   } else {
     playerInfoList.value = [];
   }
@@ -37,10 +46,13 @@ provide('changeCurrentTab', changeCurrentTab);
 </script>
 
 <template>
-  <div w-full>
+  <div class="w-full">
     <Transition name="fade-slide" mode="out-in" appear>
-      <RecordTable v-if="currentTab === 0" v-bind="{ ...pagination, name }" />
-      <RecordVideo v-else v-bind="{ playerInfoList }" />
+      <component
+        :is="currentTabComponent"
+        v-bind="currentTab === 0 ? { ...pagination, name } : { playerInfoList }"
+        @change-tab="changeCurrentTab"
+      />
     </Transition>
   </div>
 </template>
