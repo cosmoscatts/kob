6084f873ee18fc7735cfde437f550815b910b179
泠韵 <2138889191@qq.com>
2024-10-13 23:02:54 +0800

    add lastupdate scripts

--- kob-web/src/components/GameMap.vue
+++ kob-web/src/components/GameMap.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { GameMap } from '~/scripts/map';
+import { GameMap } from '~/game/map';
 
 const refParentEl = ref<HTMLElement>();
 const refCanvas = ref<HTMLCanvasElement>();
