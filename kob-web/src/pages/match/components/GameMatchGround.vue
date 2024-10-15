36a32b0e86036ff59884d06538a63e129f9f42d4
cosmoscatts <2138889191@qq.com>
2024-10-15 13:17:41 +0800

    Update

--- kob-web/src/pages/match/components/GameMatchGround.vue
+++ kob-web/src/pages/match/components/GameMatchGround.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
 import { GameControllerOutline } from '@vicons/ionicons5';
+import MatchPlayerAvatar from './MatchPlayerAvatar.vue';
 
 const router = useRouter();
+
+const { contentStyle } = useLayoutStyle({ heightProperty: 'minHeight', additionalOffset: 8 });
 
 const { user } = storeToRefs(useUserStore());
 const { opponent, socket } = storeToRefs(usePkStore());
@@ -52,8 +55,8 @@ function navigateTo(path: string) {
 </script>
 
 <template>
-  <div w60vw mxa ha flex="col">
-    <div flex-y-center justify-between mb15px>
+  <div w60vw mxa ha flex="col" :style="contentStyle">
+    <div flex-y-center justify-between mb-10px>
       <div text="primary 30px" font-800>
         匹配对抗
       </div>
@@ -61,9 +64,10 @@ function navigateTo(path: string) {
         返回
       </n-button>
     </div>
+
     <n-card>
       <div grid="~ cols-2" md:grid-flow-row-dense>
-        <div col-span-2 h15vh flex="col center" py2vh>
+        <div col-span-2 flex="col center" pb-10px>
           <n-alert title="注意事项" type="warning" :style="{ width: '40vw' }">
             选择亲自出马时，使用`W`、`A`、`S`、`D`控制方向.
           </n-alert>
@@ -72,51 +76,25 @@ function navigateTo(path: string) {
             :style="{ width: '20vw', textAlign: 'center', marginTop: '20px' }"
           />
         </div>
-        <div col-span-1 h40vh lt-md="col-span-2 ha py-20px">
-          <div flex="~ col center" h-full>
-            <n-avatar
-              :style="{
-                width: '100%',
-                maxWidth: '150px',
-                height: 'auto',
-                cursor: 'pointer',
-              }"
-              lt-md="!max-w-120px"
-              round
-              :src="user?.avatar"
-            />
-            <div mt-20px text-24px font-600>
-              {{ user?.name }}
-            </div>
-          </div>
+
+        <div col-span-1 lt-md="col-span-2 ha py-20px">
+          <MatchPlayerAvatar :user="user" />
         </div>
-        <div col-span-2 h10vh>
+
+        <div col-span-2>
           <div flex-center h-full>
             <n-button size="large" type="warning" text-color="white" @click="onClick">
               {{ matchBtnText }}
             </n-button>
           </div>
         </div>
-        <div col-span-1 h40vh lt-md="col-span-2 ha py-20px">
-          <div flex="~ col center" h-full>
-            <n-avatar
-              :style="{
-                width: '100%',
-                maxWidth: '150px',
-                height: 'auto',
-                cursor: 'pointer',
-              }"
-              lt-md="!max-w-120px"
-              round
-              :src="opponent?.avatar"
-            />
-            <div mt-20px text-24px font-600>
-              {{ opponent?.name }}
-            </div>
-          </div>
+
+        <div col-span-1 lt-md="col-span-2 ha py-20px">
+          <MatchPlayerAvatar :user="opponent" />
         </div>
       </div>
     </n-card>
+
     <div v-if="loading" fixed w-400px :style="{ top: '6vh', left: 'calc(50% - 200px)' }">
       <n-alert title="这么好玩的项目竟然没人玩？" type="warning" closable>
         <template #icon>
---
+++ kob-web/src/pages/match/components/MatchPlayerAvatar.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps(['user']);
+</script>
+
+<template>
+  <div flex="~ col center" h-full>
+    <n-avatar
+      :style="{
+        width: '100%',
+        maxWidth: '150px',
+        height: 'auto',
+        cursor: 'pointer',
+      }"
+      lt-md="!max-w-120px"
+      round
+      :src="user?.avatar"
+    />
+    <div mt-20px text-24px font-600>
+      {{ user?.name }}
+    </div>
+  </div>
+</template>
