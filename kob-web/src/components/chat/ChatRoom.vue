8fbc1c53a3e5d1e2b480c6163c3602140289975f
泠韵 <2138889191@qq.com>
2024-10-21 07:42:48 +0800

    Update

--- kob-web/src/components/chat/ChatRoom.vue
+++ kob-web/src/components/chat/ChatRoom.vue
@@ -6,25 +6,19 @@ const isHovered = useElementHover(el);
 <template>
   <div
     ref="el" class="group"
-    fixed bottom-60px right-0 rounded-l-32px h-36px px-8px py-0 text-14px cursor-pointer
+    fixed bottom-60px right-0 rounded-l-32px h-36px px-8px py-0 cursor-pointer
     flex items-center gap-6px select-none box-border bg-white dark:bg-hex-202020
-    border-1 border-solid border="[rgb(239,239,245)] dark:[rgba(255,255,255,0.09)]"
-    text-purple shadow-lg
-    hover="border-0 bg-gradient-to-r from-purple-100 to-purple-300 dark:bg-gradient-to-r dark:from-purple-700 dark:to-purple-500"
+    border-1 border-solid border="#ffffff0a"
+    text-hex-663399 shadow-lg dark:text-hex-9880f8 transition-all duration-200 ease-in-out
+    hover="bg-#dfd5fe dark:bg-#3f3663"
   >
-    <div
-      i-tabler:message-chatbot size-26px
-      transition-transform duration-300 ease-in-out
-      text-purple-600 dark:text-purple-300
-    />
+    <div i-tabler:brand-hipchat size-26px />
 
     <div
       v-if="isHovered"
-      font-800
-      op-0 transition-opacity duration-300 ease-in-out
-      group-hover="op-100"
+      font-1000 text-16px
     >
-      聊聊天
+      畅所欲言！
     </div>
   </div>
 </template>
